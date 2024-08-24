3b1ae5eff03ab511cfbc26aab83af7dd9b883f03
Raihan hossan <raihanhossan265@gmail.com>
2024-08-25 04:24:38 +0600

    server is done for delet, put, update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
@@ -25,18 +25,78 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("artCraftDB");
+    const userCollection = database.collection("artCraft");
+    const artCollection = client.db('artCraftDB').collection('addArtItem')
+
+    
+    // users server and mongodb side here
+    app.post('/users', async(req, res) =>{
+      const user = req.body
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    // artcraft server and mongodb side here
+    app.post('/artitems', async(req, res) =>{
+      const art = req.body
+      const result = await artCollection.insertOne(art)
+      res.send(result)
+    })
+
+    app.get('/artitems', async(req, res) =>{
+      const cursor = artCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/artitems/:id', async(req, res) =>{
+      const id = req.params.id
+      const art = {_id : new ObjectId(id)}
+      const result = await artCollection.findOne(art)
+      res.send(result)
+    })
+
+    app.delete('/artitems/:id', async(req, res) =>{
+      const id = req.params.id 
+      const deleteArt = {_id : new ObjectId(id)}
+      const result = await artCollection.deleteOne(deleteArt)
+      res.send(result)
+    })
+
+    app.put('/artitems/:id', async(req, res) =>{
+      const id = req.params.id 
+      console.log('the id to update' ,id);
+      const updatedBody  = req.body
+      console.log(updatedBody);
+      const filter = {_id : new ObjectId(id)}
+      console.log('update me', filter);
+      const options = { upsert : true}
+      const updatedArt = {
+        $set: {
+          name : updatedBody.name,
+          brand : updatedBody.brand,
+          type : updatedBody.type,
+          price : updatedBody.price,
+          details : updatedBody.details,
+          photo : updatedBody.photo
+        }
+      }
+      const result = await artCollection.updateOne(filter, updatedArt, options)
+      res.send(result)
+    })
+    
+    
 
 
 
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
